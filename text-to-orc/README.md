4a3b7be2c9f4eea2bef2518efb4c11b22fbf979f
David W. Streever <david@streever.com>
2014-09-18 19:48:57 -0600

    Consolidated the Script.  Now there is only one, and it generates all the needed artifacts.

--- text-to-orc/README.md
+++ text-to-orc/README.md
@@ -4,27 +4,26 @@ This set of scripts will help build the tables and conversion SQL to migrate fro
 
 These scripts do NOT use the Hive CLI, they tap directly into the Hive Metastore database on MySQL.  So you will need access to the MySql database in order to run these scripts.
 
-The core scripts are written in Groovy and will require groovy in the PATH.  Although, a complementary set of shell scripts, sharing a common name, can be used to minimize the required commandline parameters.  To use these shell scripts, the [util-env.sh] file needs to be updated to include the location and credentials for the MySql database used to store the hive metastore.
-
-The listed scripts write results to stdout, so use [io redirects|http://www.tldp.org/LDP/abs/html/io-redirection.html] to build files that will be run via 'hive -f ..'
+The script is written in Groovy and will require groovy in the PATH.  Although, a complementary set of shell scripts, sharing a common name, can be used to minimize the required commandline parameters.  To use these shell scripts, the [util-env.sh] file needs to be updated to include the location and credentials for the MySql database used to store the hive metastore.
 
 Use "--help" to get a list of options for the associated script.
 
 ## Selecting a user to run the resulting scripts
 
-Once the scripts below are used to create the artifacts for the conversion, these scripts should be run by the user that owns the table.
+Once the script below is used to create the artifacts for the conversion, the generated script should be run by the user that owns the table. Since the user will be the one that runs the Hive CLI which processes the generate DDL and DML.
 
 ## Workflow
 
 The scripts require a _hive database_ name, along with a comma separated list of _hive tables_ in the listed database.
 
-1. [ctl.sh] or [ctl.groovy] is used to create an alternate table, based on the original source table with an "ORC" format.  This process will only pull tables that are "TEXTFILE" tables in hive.
-2. Validate that the table(s) have been created.  The new table name(s) will be in the same database, with the same name with an added "_orc" extension.
-3. [migrate.sh] or [migrate.groovy] is used to build the hive scripts that will translate data from the original "TEXTFILE" formatted hive table, to the "ORC" formatted replacement.  The "ORC" table from the script above should already be created, prior to running this script.
-4. Validate that the new table(s) have been populated and that the partitions (if it's a partitioned table) exist in the metastore. 
+[convert.sh] or [convert.groovy] is used to create alternate table definition(s), based on the original source table with an "ORC" format.  This process will only pull tables that are "TEXTFILE" tables in hive. A required element of the script is an _output_ directory.  In this directory, the script will build a single DDL script that will be used to create all of the alternate tables.  A sub-directory for each table listed in the commandline will created and in it will be 1 of more scripts that will manage the movement of data between the original table and the new table.
+
+A control script named *control.sh* will be created the will call the generated scripts in the proper order.  There is an option to have this script create "nohup" jobs.  For large tables or several tables, this is recommended to increase the parallelism of the process.  Otherwise, the script will be run in a sequential manner and may never utilize the full capacity of your cluster.
+
+On the other hand, be careful how many tables (and know their sizes) you use per run, as this is also a way to manage the amount of work you push out to the cluster.
 
 ## Partition Transfers
 
-Step #3 above will construct the migration scripts with ranges, when the tables are partitioned.  The number of partitions included 'a' transfer statement can be controlled via the '-pp' option in [migrate.groovy].
+For tables with partitions, the generated scripts with be build out by ranges.  The number of partitions included 'in' a transfer statement can be controlled via the '-pp' option in [convert.groovy].
 
 With '-pp', only the first partition element is considered.  The calculated range is based on the number of unique 'first' partition elements.  For example: If the table has two partition elements like month and day, if -pp is 4, then the range will be across 4 months, regardless of the number of day partitions. 
--- text-to-orc/convert.groovy
+++ text-to-orc/convert.groovy
@@ -13,8 +13,8 @@ def cli = new CliBuilder()
 cli.hd(longOpt: 'hive-database', args: 1, required: true, 'Database to include, REQUIRED')
 cli.ht(longOpt: 'hive-tables', args: Option.UNLIMITED_VALUES, valueSeparator: ',', required: true, 'Comma separated list of tables, REQUIRED')
 cli.pp(longOpt: 'parallel-partitions', args: 1, required: false, 'Parallel Partition Count to process in statement(default 30)')
-cli.nohup(longOpt: 'nohup', args: 1, required: false, 'Build scripts to run each load sql in it\'s nohup thread')
-cli.output(longOpt: 'output-dir', args: 1, required: false, 'Output Directory for nohup scripts')
+cli.nohup(longOpt: 'nohup', args: 1, required: false, 'Build scripts to run each load sql in its nohup thread')
+cli.output(longOpt: 'output-dir', args: 1, required: true, 'Output Directory for scripts')
 cli.mh(longOpt: 'metastore-host', args: 1, required: true, 'Metastore Database Host')
 cli.mu(longOpt: 'metastore-user', args: 1, required: true, 'Metastore Database Username')
 cli.mp(longOpt: 'metastore-password', args: 1, required: true, 'Metastore Database Password')
@@ -36,24 +36,22 @@ TYPE_POSTFIX = "_orc"
 
 def database = options.hd
 
-def outputdir
 def controlfile
 
 // Cleanup and/or prep for new output.
-if (options.nohup.asBoolean() == true) {
-    if (!options.output) {
-        println "Need to specify an output directory to write scripts and sql for 'nohup' option."
-        return -1
-    }
-    outputdir = new File(options.output);
-    if (outputdir.exists()) {
-        println "Deleting existing Directory"
-        outputdir.delete()
-    }
-    outputdir.mkdir();
-    controlfile = new File(options.output+"/control.sh")
-    return 0
+
+def outputdir = new File(options.output);
+// Refresh
+if (outputdir.exists()) {
+    println "Deleting existing Directory"
+    outputdir.delete()
 }
+outputdir.mkdir();
+
+// Create DDL File
+
+// DDL Cmd Array
+def ddl_statements = []
 
 /*
 def fields = ["a":"1", "b":"2", "c":"3"]
@@ -66,20 +64,80 @@ new File("foo.ini").withWriter { out ->
 
 HIVE_SET="set hive.exec.dynamic.partition=true;\n" +
 "set hive.exec.dynamic.partition.mode=nonstrict;"
+def controlcmds = []
 
 // Append "s" to the "t" to get all... i know, crazy, right...
 options.hts.each { intable ->
     //println "${intable}"
 
-    if (options.nohup.asBoolean() != true) {
-        // Prepare for dynamic queries.
-        println $HIVE_SET
+    // Build DDL
+    sql.eachRow("select db.name, t.tbl_id, t.tbl_name, t.tbl_type, s.input_format, s.location from " +
+            "DBS db inner join TBLS t on db.db_id = t.db_id inner join SDS s on t.sd_id = s.sd_id where s.input_format = 'org.apache.hadoop.mapred.TextInputFormat' and db.name='${database}' and t.tbl_name='${intable}'") { table ->
+//    println "$table.name, $table.tbl_name, $table.tbl_type, $table.input_format, $table.location"
+        println "USE $database;"
+        def CREATE_STATEMENT
+        if ("$table.tbl_type" == "EXTERNAL_TABLE") {
+            CREATE_STATEMENT = "CREATE EXTERNAL TABLE IF NOT EXISTS $table.tbl_name" + TYPE_POSTFIX + " (\n"
+        } else {
+            CREATE_STATEMENT "CREATE TABLE IF NOT EXISTS $table.name" + "." + "$table.tbl_name" + TYPE_POSTFIX + " (\n"
+        }
+        def columns = []
+        sql.eachRow("select c2.column_name, c2.type_name from " +
+                "TBLS t inner join SDS s on t.sd_id = s.sd_id inner join CDS c on s.cd_id = c.cd_id inner join COLUMNS_V2 c2 on c.cd_id = c2.cd_id " +
+                "where t.tbl_id = $table.tbl_id order by c2.integer_idx") { column ->
+            columns.add("$column.column_name $column.type_name")
+        }
+        COLUMNS = ""
+        columns.each { column ->
+            COLUMNS = COLUMNS + "   " + column;
+            if (column != columns.last()) {
+                COLUMNS = COLUMNS + ",\n"
+            }
+        }
+        CREATE_STATEMENT = CREATE_STATEMENT + COLUMNS + ")\n"
+
+        // Partitions
+        def partitions = []
+        PARTITIONS = ""
+        sql.eachRow("select p.pkey_name, p.pkey_type from TBLS t inner join PARTITION_KEYS p on t.tbl_id = p.tbl_id where t.tbl_id = $table.tbl_id order by p.integer_idx; ") { partition ->
+            partitions.add("$partition.pkey_name $partition.pkey_type")
+        }
+        partitions.each { partition ->
+            PARTITIONS = PARTITIONS + "   " + partition;
+            if (partition != partitions.last()) {
+                PARTITIONS = PARTITIONS + ",\n"
+            }
+        }
+        if (PARTITIONS.length() > 0) {
+            CREATE_STATEMENT = CREATE_STATEMENT + "PARTITIONED BY (\n" + PARTITIONS + "\n)\n"
+        }
+
+        // LOCATION if EXTERNAL
+        if ("$table.tbl_type" == "EXTERNAL_TABLE") {
+            // STORED AS
+            CREATE_STATEMENT = CREATE_STATEMENT + "STORED AS ORC\n"
+            location = "$table.location" + "_orc"
+            CREATE_STATEMENT = CREATE_STATEMENT +  "LOCATION '" + location + "';"
+        } else {
+            // STORED AS
+            CREATE_STATEMENT = CREATE_STATEMENT + "STORED AS ORC;"
+        }
+
+        ddl_statements.add(CREATE_STATEMENT)
     }
+
+
+//    if (options.nohup.asBoolean() != true) {
+//        // Prepare for dynamic queries.
+//        println HIVE_SET
+//    }
 
     sql.eachRow("select db.name, t.tbl_id, t.tbl_name, t.tbl_type, s.input_format, s.location from " +
             "DBS db inner join TBLS t on db.db_id = t.db_id inner join SDS s on t.sd_id = s.sd_id where s.input_format = 'org.apache.hadoop.mapred.TextInputFormat' and db.name='${database}' and t.tbl_name='${intable}'") { table ->
 
-        println "USE $database;"
+
+//        if (options.nohup.asBoolean() != true)
+//            println "USE $database;"
 
         def fields = []
         sql.eachRow("select c2.column_name, c2.type_name from " +
@@ -131,6 +189,7 @@ options.hts.each { intable ->
             if (partitions.size() > 0) {
                 // YES, we have partitions.
                 int part_count = 0;
+                int part_file_count = 0;
                 // ? Handling List of Maps....
                 // Output should be like:
                 //   part >= '...' and part < '...'
@@ -148,7 +207,23 @@ options.hts.each { intable ->
                     if (actual_partition == partitions.last()) {
                         // Last partition, special Handling.
                         wherePart.add(key + " <= '" + value + "'")
-                        println INSERT_STATEMENT + "   WHERE " + wherePart[0] + " and " + wherePart[1] + ";"
+                        INSERT_STATEMENT_WITH_WHERE = INSERT_STATEMENT + "\n   WHERE " + wherePart[0] + " and " + wherePart[1] + ";"
+                        part_file_count++;
+                        // Create output file for partition set.
+                        def partfile_name = intable+"/part_"+part_file_count+".sql"
+                        if (! new File(options.output+"/"+intable).exists())
+                            new File(options.output+"/"+partfile_name).mkdir()
+                        new File(options.output+"/"+partfile_name).withWriter { partfile ->
+                            // Add use..
+                            partfile.writeLine("USE $database;")
+                            // Add Insert..
+                            partfile.writeLine(INSERT_STATEMENT_WITH_WHERE)
+                            // Add to control file.
+                            controlcmds.add("hive -f $partfile_name")
+//                                controlfile.withWriter { cout ->
+//                                    cout.writeLine("nohup hive -f $partfile_name &")
+//                                }
+                        }
                     } else {
                         if (value == lastPart) {
                             // Nothing, continue to next partition.  This enables us to scan and build statements
@@ -166,7 +241,21 @@ options.hts.each { intable ->
                             if (part_count >= PARALLEL_PARTITIONS - 1) {
                                 // reset
                                 // set where based on current where part.
-                                println INSERT_STATEMENT + "   WHERE " + wherePart[0] + " and " + wherePart[1] + ";"
+                                INSERT_STATEMENT_WITH_WHERE = INSERT_STATEMENT + "\n   WHERE " + wherePart[0] + " and " + wherePart[1] + ";"
+                                part_file_count++;
+                                // Create output file for partition set.
+                                def partfile_name = intable+"/part_"+part_file_count+".sql"
+                                if (! new File(options.output+"/"+intable).exists())
+                                    new File(options.output+"/"+partfile_name).mkdir()
+                                new File(options.output+"/"+partfile_name).withWriter { partfile ->
+                                    // Add use..
+                                    partfile.writeLine("USE $database;")
+                                    // Add Insert..
+                                    partfile.writeLine(INSERT_STATEMENT_WITH_WHERE)
+                                    // Add to control file.
+                                    controlcmds.add("hive -f $partfile_name")
+
+                                }
                                 wherePart = []
                                 part_count = 0;
                             } else {
@@ -178,14 +267,35 @@ options.hts.each { intable ->
             } else {
                 // No partitions found
             }
-//            println where
+
 
         } else {
             // NO partitions defined.
         }
 
+
         // TODO: Build the swap commands.
 
 
+    }
+}
+
+ddl_file = new File(options.output+"/build_ddl.sql")
+ddl_file.withWriter { ddlout ->
+    ddlout.writeLine("use $database;")
+    ddl_statements.each { ddl ->
+        ddlout.writeLine(ddl)
+    }
+}
+controlfile = new File(options.output+"/control.sh")
+controlfile.withWriter { cout ->
+    cout.writeLine("#!/bin/bash")
+    cout.writeLine("cd `dirname \$0`")
+    cout.writeLine("hive -f build_ddl.sql")
+    controlcmds.each { cmd ->
+        if (options.nohup.asBoolean() == true)
+            cout.writeLine("nohup $cmd > "+options.output+".nohup.out &")
+        else
+            cout.writeLine(cmd)
     }
 }
